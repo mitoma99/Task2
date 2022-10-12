d5fd0f8bcd434dd16d85938e8d0a1f3f4e7963c3
mitoma99 <114859605+mitoma99@users.noreply.github.com>
2022-10-12 12:49:23 +0000

    [Update3]Task2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
+  get 'top' => 'homes#top'
   post 'books' => 'books#create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
